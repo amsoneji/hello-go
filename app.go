c16ca5730b606b437e909913c123b80f21fec732
Aniket Soneji <asoneji@ASONEJI-MBP.local>
2019-03-22 09:45:08 -0700

    Adjusting app.go to remove extraneous newline

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	hello()
-	fmt.Println("\n\nAbout to Sleep!\n")
+	fmt.Println("\n\nAbout to Sleep!")
 	time.Sleep(10 * time.Second)
 	fmt.Println("Done with Hello examples!")
 }
